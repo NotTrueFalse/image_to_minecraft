f58fab724c9f0aa5fa87ec057ce9a6a8c136ddf6
ender <122208389+NotTrueFalse@users.noreply.github.com>
2023-06-21 19:47:59 +0200

    Update main.py
    multithread update

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+#tpp = time per pixel
+#this script will convert an image to minecraft items
+#it will find the nearest minecraft item for each pixel
+#it will then create a new image with the minecraft items
+#it will also create a json file with the minecraft item colors
+#this script will use multithreading to speed up the process
 from PIL import Image
 import os
-import random
 import json
+import time
+import threading
+import tkinter as tk
 
-SCALE_BY = 64
-ALPHA = False
+SCALE_BY = 32
+ALPHA = True
+THREAD_COUNT = 10    #how many threads to use
+ORIGINAL_SCALE = 0.8 #scale the image down to this size before converting to minecraft items
 
 def pixel_to_int(p):
     if not isinstance(p, tuple):
@@ -50,14 +60,74 @@ except:
     json.dump(ending, open('minecraft_item_color.json', 'w'))
 
 #ask for an image, and then find the closest minecraft item for each pixel
-im = Image.open(input('Image: '))
+try:
+    im = Image.open(input('Image: '))
+except:
+    print('Invalid image')
+    exit()
+im = im.resize((int(im.size[0]*ORIGINAL_SCALE),int(im.size[1]*ORIGINAL_SCALE)))
 w_, h_ = im.size
 w_, h_ = w_ * SCALE_BY, h_ * SCALE_BY
 out_im = Image.new('RGBA', (w_,h_))
-for x in range(im.size[0]):
-    for y in range(im.size[1]):
-        pixelint = pixel_to_int(im.getpixel((x, y)))
-        closest = random.choice(ending[get_nearest_color(pixelint)])
-        out_im.paste(Image.open(closest).resize((SCALE_BY, SCALE_BY)), (x*SCALE_BY, y*SCALE_BY))
+stats = {"time":{},"pixel_count":0}
+pixeldata = im.getdata()
+final_pixel_data = [""]*len(pixeldata)
+
+def convert_pixel_to_nearest(pixel):
+    global ending
+    nearest = ending[get_nearest_color(pixel_to_int(pixel))][0]
+    nearest = Image.open(nearest)
+    nearest = nearest.resize((SCALE_BY,SCALE_BY))
+    return nearest
+
+def threader(data_sample:list,thread_id:int,glob_start:int):
+    global stats
+    global im
+    if not thread_id in stats["time"]:
+        stats["time"][thread_id] = 0
+    #loop through each pixel in the image and find the nearest img then add it to final_pixel_data[start+i]
+    for i in range(len(data_sample)):
+        start = time.time()
+        final_pixel_data[glob_start+i] = convert_pixel_to_nearest(data_sample[i])
+        stats["time"][thread_id] = time.time()-start
+        stats["pixel_count"] += 1
+        if i == len(data_sample)-1:
+            print(f"[+] Thread {thread_id} finished")
+
+window = tk.Tk()
+window.title("Progress")
+window.geometry("300x400")
+progress = tk.Label(window,text="Progress will be shown here")
+progress.pack()
+avg_progress = [tk.Label(window,text=f"Thread {i} avg time per pixel: 0s") for i in range(THREAD_COUNT)]
+#create the number of threads
+t1 = time.time()
+for i in range(THREAD_COUNT):
+    start,end = i*int(len(pixeldata)/THREAD_COUNT),(i+1)*int(len(pixeldata)/THREAD_COUNT)
+    sample_data = list(pixeldata)[start:end]
+    t = threading.Thread(target=threader, args=(sample_data,i,start))
+    avg_progress[i].pack()
+    t.daemon = True
+    t.start()
+    time.sleep(0.1)
+#wait for all threads to finish
+print(f"[+] Started {THREAD_COUNT} threads")
+while threading.active_count() > 1:
+    perc = round(stats["pixel_count"]/(im.size[0]*im.size[1])*100,2)
+    avg = stats["time"].values()
+    for i in range(len(avg_progress)):
+        avg_progress[i].config(text=f"Thread {i} tpp: {round(list(avg)[i],4)}s")
+    progress.config(text=f"[*] {perc}% active: {threading.active_count()-1}")
+    window.update()
+    time.sleep(0.1)
+#create the final image
+for i in range(len(final_pixel_data)):
+    try:
+        out_im.paste(final_pixel_data[i],(i%im.size[0]*SCALE_BY,i//im.size[0]*SCALE_BY))
+    except:
+        pass
+window.destroy()
+print(f"[+] Finished in {round(time.time()-t1,2)}s")
 out_im.save('output.png')
 im.close()
+print(f"[+] Saved output.png")
